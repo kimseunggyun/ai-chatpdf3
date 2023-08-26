1a936ddd92efe9c7bcc45dba6a94cda7bd1ec7b6
kimseunggyun <wolfchun4@naver.com>
2023-08-26 21:05:23 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+__import__('pysqlist3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+# from dotenv import load_dotenv
+# load_dotenv()
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+#from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains import RetrievalQA
+import streamlit as st
+import tempfile
+import os
+
+#제목
+st.title("ChatPDF")
+st.write("---")
+
+#파일 업로드
+uploaded_file = st.file_uploader("Choose a file", type=['pdf'])
+st.write("---")
+
+def pdf_to_document(uploaded_file):
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
+    pages = loader.load_and_split()
+    return pages
+
+#업로드 되면 동작하는 코드
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file)
+    pass
+
+#SPLIT
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 20,
+    length_function = len,
+    is_separator_regex=False,
+)
+
+texts = text_splitter.split_documents(pages)
+
+#embedding
+embeddings_model = OpenAIEmbeddings()
+
+# load it into Chroma
+db = Chroma.from_documents(texts, embeddings_model)
+
+#question
+st.header("PDF에게 출력해보세요!!")
+question = st.text_input('질문을 입력하세요')
+
+if st.button('질문하기'):
+    with st.spinner('Wait for it...'):
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        qa_chain = RetrievalQA.from_chain_type(llm, retriEver=as_retriever())
+        result = qa_chain({"query": question})
+        st.write(result)
+
+# retriver_from_llm = MultiQueryRetriver.from_llm(
+#     retriver=db.as_retriver(), llm=llm
+# )
+
+# docs = retriver_from_llm.get_retrivant_document(query=question)
+
+# print(len(docs))
+# print(docs)
+
+
+
